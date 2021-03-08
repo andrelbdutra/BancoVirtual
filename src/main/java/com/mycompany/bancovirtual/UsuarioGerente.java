0c2877ee4081a2a79df363f306822ba5ea0b7709
sharpenerhd@hotmail.com <Sharpenerhd@hotmail.com>
2021-03-08 20:57:35 -0300

    criando e modificando a classe UsuarioGerente

--- src/main/java/com/mycompany/bancovirtual/UsuarioGerente.java
+++ src/main/java/com/mycompany/bancovirtual/UsuarioGerente.java
@@ -9,6 +9,18 @@ package com.mycompany.bancovirtual;
  *
  * @author Pichau
  */
-public class UsuarioGerente {
-    
+public class UsuarioGerente extends Usuario {
+
+    private int senha;
+    private int numeroDeFuncionariosGerenciados;
+
+    public boolean autentica(int senha) {
+        if (this.senha == senha) {
+            System.out.println("Acesso Permitido!");
+            return true;
+        } else {
+            System.out.println("Acesso Negado!");
+            return false;
+        }
+    }
 }
